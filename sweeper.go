5f829048be968128baae576cc514fc23cabe7c48
Christian <teeswizzle19@gmail.com>
2017-12-31 21:36:21 -0500

    wip frustrated refactoring - got execing spaces working - TODO recurse on directory

--- sweeper.go
+++ sweeper.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"log"
-	"os/exec"
-	"strings"
-	"fmt"
+    "os/exec"
+    "os"
+    "strings"
+    "fmt"
+    "log"
+    "strconv"
 )
 
 // env GOOS=darwin GOARCH=amd64 go build -o dist/sweeper-darwin-amd64  sweeper.go
@@ -12,43 +14,99 @@ import (
 // env GOOS=linux GOARCH=amd64 go build -o dist/sweeper-linux-amd64  sweeper.go
 // env GOOS=windows GOARCH=amd64 go build -o dist/sweeper-windows-amd64  sweeper.go
 
-func main() {
-	// command to get rid of any alias to ensure format
-	// split by newline - for each, string.Fields to get fields - find way to deal with space(s)! in the name - recurse if directory
-	// what happens if you dont have access?
-	cmd := exec.Command("command", "ls", "-l")
-	log.Printf("Running command...")
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Command ran!")
-
-	spl := strings.Split(string(out), "\n")
-	take := spl[1 : (len(spl) - 1)]
-	var contents [][9]string
-
-	for _, line := range take {
-		var info [9]string
-
-		fields := strings.Fields(line)
-
-		// account for spaces before and after file/folder name
-		// kinda ugly, and not that efficient, but works
-		name := strings.Split(line, (fields[7] + " "))[1]
-		copy(info[0:8], fields[0:8])
-		info[8] = name
-
-		contents = append(contents, info)
-		//fmt.Printf("%#v\n", info)
-	}
-
-
-	// fmt.Printf("%#v\n", contents)
-	// print so i can see
-	fmt.Printf("[\n")
-	for _, c := range contents {
-		fmt.Printf("\t%#v\n", c)
-	}
-	fmt.Printf("]\n")
+type Info struct {
+    directory   bool
+    owner       string
+    group       string
+    size        uint64
+    name        string
 }
+
+func pprint(contents []Info) {
+    fmt.Printf("[\n")
+    for _, c := range contents {
+        fmt.Printf("\t%#v\n", c)
+    }
+    fmt.Printf("]\n\n")
+}
+
+func scanContents(location string) ([]Info, uint64) {
+    var total_size uint64 = 0
+
+    fmt.Printf("Scanning %s ...\n", location)
+
+    // command to get rid of any alias to ensure format
+    cmd := exec.Command("command", "ls", "-l", location)
+    out, err := cmd.Output()
+    if err != nil {
+        fmt.Printf("%s\n", cmd.Stderr)
+        log.Fatal(err)
+    }
+
+    spl := strings.Split(string(out), "\n")
+    take := spl[1 : (len(spl) - 1)]
+    var contents []Info
+
+    for _, line := range take {
+
+        fields := strings.Fields(line)
+        // account for spaces before and after file/folder name
+        // kinda ugly, and not that efficient, but works
+        name := strings.Replace(strings.Split(line, (fields[7] + " "))[1], " ", "\\ ", -1)
+        fmt.Printf("NAME: %s\n", name)
+
+        directory := (fields[0][0] == 'd')
+
+        var size uint64
+        if directory {
+            _, size = scanContents(location + "/" + name)
+        } else {
+            size, err = strconv.ParseUint(fields[4], 10, 64)
+            if err != nil {
+                log.Fatal(err)
+            }
+        }
+        // add to total size for current directory
+        total_size += size
+
+        info := Info{   
+                        directory : directory,
+                        owner     : fields[2],
+                        group     : fields[3],
+                        size      : size,
+                        name      : name,
+                    }
+
+        contents = append(contents, info)
+    }
+
+    pprint(contents)
+
+    return contents, total_size
+}
+
+func main() {
+    dir, err := os.Getwd()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // _, size := scanContents(dir)
+    // fmt.Printf("Size of that Directory: %d\n", size)
+
+    // so apparently you need the space to be ascii code... rip 90 minutes
+    cmd := exec.Command("command", "ls", "-l", "\x20\x20\x20\x20test\x20\x20\x20\x20folder\x20\x20\x20\x20")
+    fmt.Printf("%#v \n", cmd.Args)
+
+    out, err := cmd.Output()
+    if err != nil {
+        fmt.Printf("%s\n", cmd.Stderr)
+        log.Fatal(err)
+    }
+    fmt.Printf("%s\n%s\n", dir, out)
+
+    // what happens if you dont have access?
+    // json?
+}
+
+
